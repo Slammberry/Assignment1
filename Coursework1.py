7e98d1ff15f1d8895c2bed43b3d59118a30b9f82
Slammberry <1714404@brunel.ac.uk>
2019-01-28 05:50:58 +0000

    More content
    Crossroad loops added

--- Coursework1.py
+++ Coursework1.py
@@ -28,8 +28,9 @@ def Game():
                 print('\nI dinee catch that?')
         break
 
-    inventory = {"Dagger": 0, "BluePotions": 0, "GoldenFeathers": 0, "Manuscrpits": 0}
+    inventory = {"Dagger": 0, "BluePotions": 0, "GoldenFeathers": 0, "Manuscrpits": 0, "Shield": 0}
     abilities = {"StealthPoints": 0, "FlyPoints": 0, "PathFinder": 0, "PathFinderTwo": 0}
+    effects = {"Slow": 0}
     while True:
         actionOne = input("You walk through a forest and see a BluePotion, do you pick it up? Y/N: ")
         if actionOne == "y":
@@ -78,7 +79,39 @@ def Game():
     while True && if abilities['PathFinder'] == 1:
         actionOneForward = input("You start to notice that it's a slightly suspicous that noone is around, and as you walk it feels more like a mistake you came out here. You look up and you realise why there's noone around; you have walked into a hunting ground for dragons and you are now their prey! Do you run or hide?")
         if actionOneForward == "hide" && abilities["StealthPoints"]>=1
-            print('')
+            inventory['Shield']+=1
+            dragonSafe = input('You find a crack to slip into and hide there, after a couple of minutes of searching the dragons give up; they don\'t have time to waste on a small fry like you. You go back to the crossroads glad you escaped with your life. On your way back you find a dragon scale, it\'s enormous and might serve as a shield. Back at the crossroads you must choose left, right, or back?')
+                if actionCrossroadsOne == "back"
+                    print('You\'re right, the world is a scary place and it\'s best not to leave your comfort zone...' )
+                    abilities['PathFinder']=2
+                    break
+                if actionCrossroadsOne == "left"
+                    print('You consider yourself a risk taker; you eat fear for breakfast')
+                    abilities['PathFinder']=3
+                    break
+                if actionCrossroadsOne == "right"
+                    print('This path seems the least ominous, the vibrant life around you makes you feel very relaxed')
+                    abilities['Pathfinder']=4
+                    break
+                if actionCrossroadsOne == "..."
+                    print('\n...?')
+                    break
+        if actionOneForward == "hide" && abilities["StealthPoints"]<=0
+            print("There is nowhere to hide in this completely empty field.")
+            Death("You are now doomed dragon dinner.")
+        if actionOneForward == "run"
+            print("You try to run but this only draws more attention to you, the dragons are on you like a hawk on it\'s prey. It\'s over.")
+            Death("You are now doomed dragon dinner.")
+    while True && if abilities['PathFinder'] == 2:
+        actionOneBack = input('Truly this adventure wasn\'t meant for you but as you walk back you realise that you are no longer on the path that you started on; deep fog begins to roll in and you soon find you are completely lost. Do you run or wait?')
+        if actionOneBack == ('run')
+            print('You run wildly in hope that you get out of this fog to eventually pop out at the crossroads once more feeling much heavier than sluggish than before.')
+            effects["Slow"]+=1
+            break
+        if actionOneBack == "wait"
+            print('You fear the worst so you wait for the fog to roll out again...and you wait...and wait.')
+            Death('You let fear control you and it became your doom...')
+        
     a =input()
 
 Game()
