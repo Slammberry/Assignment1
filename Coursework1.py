e387471f72403c1c41308550762c76c018b33ec7
Slammberry <1714404@brunel.ac.uk>
2019-01-28 08:20:37 +0000

    Complete Untested
    My home PC won't allow me to run python so I have no idea if it works

--- Coursework1.py
+++ Coursework1.py
@@ -55,7 +55,7 @@ def Game():
         if actionTwo == '...':
             print('\n...?')
             break
-    while True:
+    while True && if abilities['PathFinder'] == 0:
         actionCrossroadsOne = input("After walking for some time you come to a crossroads. Ahead of you you see a break in the forest scenery into a field like grassland area, to your right and left you see opposing forests; one full of life and wonder and the other dark and haunting respectively. Behind you is the path you have trodden so far. Do you go forward, back, left or right?")
         if actionCrossroadsOne == "forward"
             print('It feels good to get out of the forest, a nice open area with nothing around except you and the the sky...')
@@ -154,7 +154,46 @@ def Game():
                 if surviveDeath == "..."
                     print('\n...?')
                     break
-    
+    while True && if abilities['PathFinder'] == 4:
+        actionOneRight = input('You feel incredibly relaxed in this paradise, each step you take puts a bigger smile on your face. Your movement begins to slow even more as you take in your surroundings. You realise this uphoria is unnatural, you are being drugged. You must act fast; do you retreat or relax?')
+        if actionOneRight == "retreat" && effects["slow"]>=1
+            print("The drugs and the weight in your legs gets to be much for you to handle. You collapse onto the floor.")
+            Death("You are now part of the forest floor and are one with this garden of eden.")
+        if actionOneRight == "retreat" && effects["slow"]<=0
+            escapeHeaven = input("Your drowsied state doesn\'t calm your fast reactions, you bolt back down the path you were on to the safety of the crossroads. When you reach it you are ganted fresh air and a choice of path, you can't help but miss the happieness that place granted you however.")
+            if escapeHeaven == "forward"
+                print('It feels good to get out of the forest, a nice open area with nothing around except you and the the sky...')
+                abilities['PathFinder']=1
+                break
+            if escapeHeaven == "back"
+                print('You\'re right, the world is a scary place and it\'s best not to leave your comfort zone...' )
+                abilities['PathFinder']=2
+                break
+            if escapeHeaven == "left"
+                print('You consider yourself a risk taker; you eat fear for breakfast')
+                abilities['PathFinder']=3
+                break
+            if escapeHeaven == "right"
+                print('This path seems the least ominous, the vibrant life around you makes you feel very relaxed')
+                abilities['Pathfinder']=4
+                break
+            if escapeHeaven == "..."
+                print('\n...?')
+                break
+        if actionOneRight == "relax"
+            trueLove = input('As you relax you feel no regret in your decision; you sit off the path and lean against a nearby tree. If this is to be the end you are ok with that, your eyes slowly close and you are ready for whatever comes next with open arms. As you awake you realise you are no longer on the forest floor but atop a gigantic tree with a somewhat curious, beautiful elven girl staring at you from a distance. \"Hello...do you speak English\" you ask her, \"yes...I do speak your tongue human but don\'t get cocky because I let you live. You seemed to love this land as much I did was all, but I DO NOT trust you.\" she replied. Very curious indeed an elf helping a human. Before you have time to contemplate your thoughts a wild dragon appears from below the treeline and is poised to roast the unsuspecting elven girl from behind. You must do something and now; do you block, push or run?')
+            if trueLove == "block" && inventory['Shield']>=1
+                print("Your dragon scale shield is flame resistant...obviosly. When the dragon loses it\'s element of surprise and realises its flames are useless it panics and retreats. Having saved the girl you introduce yourself, \"I'm" +name+ "by the way what\'s your name.\" Flustered and embarrassed she replies \"I\'m Evelyn but you can call me Eve\". Lost for words you both begin chuckling like lovestruck teens, with time you and Eve get to know one another and you spend the rest of your life in that forest getting to know that woman that your children would one day call mother")
+                Death("All stories must come to an end and you are overwhelmed with joy that yours was so full of laughter and love.")
+            if trueLove == "block" && inventory['Shield']<=0
+                print("With nothing to block with your efforts are fruitless as you and the mystery girl are turned to ash in moments.")
+                Death("Was it bravery or stupidity that led you here?")
+            if trueLove == 'push'
+                print("You are lost for what to do, still feeling endebted to the girl you push her out of the way to save her and as the flames consume you you see a silent thanking in the elfs eyes")
+                Death("Chivalry being dead is debatable, you on the other hand most certainly are.")
+            if trueLove == 'run'
+                print("You are lost for what to do, fearing for your own life you jump back as you see the flames consume the girl you watch as she tears up with a look of betrayal as she is no more. You escape the forest but that memory haunts you for the rest of your life, never letting you find happieness again.")
+                Death("You die alone living a life full of remorse and regret.")
     a =input()
 
 Game()
